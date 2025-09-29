18f10100fa33620e3542279a422ae894b7f60a3f
Satya Sankalp Gowravajhal <86484291+SatyaSankalp0402@users.noreply.github.com>
2025-09-29 11:14:13 +0530

    Update PaymentSystem.py

--- PaymentSystem.py
+++ PaymentSystem.py
@@ -1,8 +1,11 @@
 from enum import Enum
-from PaymentHandlers import ProcessPayPal
+import PaymentHandlers 
 class PaymentMode(Enum):
   PayPal="PAYPAL"
+  GooglePay="GOOGLEPAY"
 
 def checkout(mode,amount):
   if mode.upper()==PaymentMode.PayPal.value:
     return ProcessPayPal(amount)
+  elif mode.upper()==PaymentMode.GooglePay.value:
+    return ProcessGooglePay(amount)
