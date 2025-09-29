08b3a723d6f642d6b8438579fab6e5af7e62c22b
Satya Sankalp Gowravajhal <86484291+SatyaSankalp0402@users.noreply.github.com>
2025-09-29 11:11:29 +0530

    Update PaymentSystemTest.py

--- PaymentSystemTest.py
+++ PaymentSystemTest.py
@@ -6,3 +6,6 @@ class TestCheckout(unittest.TestCase):
     self.assertEqual(result,"Processed PayPal payment amount of $100")
     result=checkout("Paypal",150)
     self.assertEqual(result,"Processed PayPal payment amount of $150")
+  def test_checkout_googlepay(self):
+    result=checkout("GooglePay",200)
+    self.assertEqual(result,"Processed GooglePay payment amount of $200")
