70cf74bc988fccf86b4a9315fabaf344047e5252
Satya Sankalp Gowravajhal <86484291+SatyaSankalp0402@users.noreply.github.com>
2025-09-29 11:33:41 +0530

    Update PaymentSystem.py

--- PaymentSystem.py
+++ PaymentSystem.py
@@ -8,10 +8,9 @@ class PaymentMode(Enum):
   CreditCard="CREDITCARD"
 
 def checkout(mode,amount):
-  payment_methods = 
-  {  
-    PaymentMode.PayPal.value: ProcessPayPal
-    PaymentMode.GooglePay.value: ProcessGooglePay
+  payment_methods = {  
+    PaymentMode.PayPal.value: ProcessPayPal,
+    PaymentMode.GooglePay.value: ProcessGooglePay,
     PaymentMode.CreditCard.value: ProcessCreditCard
 }
 method=payment_methods.get(mode.upper())
