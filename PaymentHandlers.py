1295cbb6ff101a564e7c851d24800337c912b2dc
Satya Sankalp Gowravajhal <86484291+SatyaSankalp0402@users.noreply.github.com>
2025-09-29 11:14:34 +0530

    Update PaymentHandlers.py

--- PaymentHandlers.py
+++ PaymentHandlers.py
@@ -1,6 +1,6 @@
 def ProcessPayPal(amount):
   return f"Processed PayPal payment amount of ${amount}"
   #Add PayPal Specific Logic Here
-def GooglePay(amount):
+def ProcessGooglePay(amount):
   return f"Processed GooglePay payment amount of ${amount}"
   #Add GooglePay Specific Logic Here
