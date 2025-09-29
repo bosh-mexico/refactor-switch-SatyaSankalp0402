9ce874a78eb2359cd6a53fbb6a661d8b967d9c1f
Satya Sankalp Gowravajhal <86484291+SatyaSankalp0402@users.noreply.github.com>
2025-09-29 10:44:06 +0530

    Update PaymentSystemTest.py

--- PaymentSystemTest.py
+++ PaymentSystemTest.py
@@ -2,6 +2,6 @@ import unittest
 from PaymentSystem import checkout
 class TestCheckout(unittest.TestCase):
   def test_checkout_paypal(self):
-    result=checkout(PayPal,100)
+    result=checkout("PayPal",100)
     self.assertEqual(result,"Processed 100.0 using PayPal")
   
