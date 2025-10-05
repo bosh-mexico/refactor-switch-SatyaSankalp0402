0bc89d4dc2f2171a954a7ee0531ab3a689e2431b
Satya Sankalp Gowravajhal <86484291+SatyaSankalp0402@users.noreply.github.com>
2025-10-05 23:33:45 +0530

    Update PaymentHandlers.py

--- PaymentHandlers.py
+++ PaymentHandlers.py
@@ -1,11 +1,15 @@
-def ProcessPayPal(amount):
-  return f"Processed PayPal payment amount of ${amount}"
-  #Add PayPal Specific Logic Here
-def ProcessGooglePay(amount):
-  return f"Processed GooglePay payment amount of ${amount}"
-  #Add GooglePay Specific Logic Here
-def ProcessCreditCard(amount):
-  return f"Processed CreditCard payment amount of ${amount}"
-  #Add CreditCard Specific Logic Here
-def InvalidPaymentMode():
-  return "Invalid Payment Mode Selected"
+class PayPalPayment:
+  def ProcessPayment(self,amount):
+    return f"Processed PayPal payment amount of ${amount}"
+    #Add PayPal Specific Logic Here
+class GooglePayPayment:
+  def ProcessPayment(self,amount):
+    return f"Processed GooglePay payment amount of ${amount}"
+    #Add GooglePay Specific Logic Here
+class CreditCardPayment:
+  def ProcessPayment(self,amount):
+    return f"Processed CreditCard payment amount of ${amount}"
+    #Add CreditCard Specific Logic Here
+class UnknownPayment:
+  def ProcessPayment(self,amount):
+    return "Invalid Payment Mode Selected!"
