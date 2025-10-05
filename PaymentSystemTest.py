0e54b18537b0f9baccfe9ad483bf5d8d79537224
Satya Sankalp Gowravajhal <86484291+SatyaSankalp0402@users.noreply.github.com>
2025-10-05 23:44:37 +0530

    Update PaymentSystemTest.py

--- PaymentSystemTest.py
+++ PaymentSystemTest.py
@@ -2,18 +2,14 @@ import unittest
 from PaymentSystem import checkout
 class TestCheckout(unittest.TestCase):
   def test_checkout_paypal(self):
-    result=checkout("PayPal",100)
+    result=checkout(PaymentMode.PAYPAL,100)
     self.assertEqual(result,"Processed PayPal payment amount of $100")
-    result=checkout("Paypal",150)
-    self.assertEqual(result,"Processed PayPal payment amount of $150")
   def test_checkout_googlepay(self):
-    result=checkout("GooglePay",200)
+    result=checkout(PaymentMode.GOOGLEPAY,200)
     self.assertEqual(result,"Processed GooglePay payment amount of $200")
-    result=checkout("gooGlePAY",150)
-    self.assertEqual(result,"Processed GooglePay payment amount of $150")
   def test_checkout_creditcard(self):
-    result=checkout("CreditCard",10000)
+    result=checkout(PaymentMode.CREDITCARD,10000)
     self.assertEqual(result,"Processed CreditCard payment amount of $10000")
   def test_checkout_invalid(self):
-    result=checkout("PhonePe",4000)
+    result=checkout(PaymentMode.UNKNOWN,4000)
     self.assertEqual(result,"Invalid Payment Mode Selected")
